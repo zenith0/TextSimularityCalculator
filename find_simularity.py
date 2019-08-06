4f85383313ed38db0e53897a34e0061f9422e9a3
Stefan Perndl <pernstef@gmail.com>
2019-08-06 16:37:20 +0200

    after data model is loaded user is prompted for a word. returns the similarities

--- find_simularity.py
+++ find_simularity.py
@@ -1,6 +1,7 @@
 import os
 from keras.utils import get_file
 import subprocess
+import gensim
 
 MODEL = 'GoogleNews-vectors-negative300.bin'
 path = get_file(MODEL + '.gz', 'https://deeplearning4jblob.blob.core.windows.net/resources/wordvectors/%s.gz' % MODEL)
@@ -16,4 +17,12 @@ if not os.path.isfile(unzipped):
                          )
         zcat.wait()
 
-        
+model = gensim.models.KeyedVectors.load_word2vec_format(unzipped, binary=True)
+
+while 1:
+    print('\n\n##### Data model successfully loaded!\n')
+    word = input('Enter a word to find similarities: ')
+    try:
+        print(model.most_similar(positive=[word]))
+    except KeyError as e:
+        print (e)
